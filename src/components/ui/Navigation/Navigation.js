9836085ea16bfa3bda5b69bbc4e7d1a4839cbd70
Godwin AVODAGBE <gavodagbe@gmail.com>
2021-03-10 10:24:40 +0100

    Add Navigation

---
+++ src/components/ui/Navigation/Navigation.js
@@ -0,0 +1,66 @@
+import { Route, NavLink, Switch } from "react-router-dom";
+import App from "./../../../App";
+import UserAdmin from "./../../container/UserAdmin/UserAdmin";
+import NewPost from "./../Blog/NewPost/NewPost";
+import Posts from "./../Blog/Posts/Posts";
+import FullPost from "./../../ui/Blog/FullPost/FullPost";
+
+const Navigation = (props) => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Training React
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link active" to="/">
+                  Acceuil
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/users">
+                  Users
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/posts">
+                  Posts
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/new-post">
+                  Nouveau post
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <Switch>
+        <Route component={App} path="/" exact />
+        <Route component={UserAdmin} path="/users" />
+        <Route path="/new-post" component={NewPost} />
+        <Route path="/posts" component={Posts} />
+        {/*<Route path="/posts/:id" component={FullPost} />*/}
+        {/*<Redirect from="/" to="/posts" />*/}
+      </Switch>
+    </div>
+  );
+};
+
+export default Navigation;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import UserAdmin from "./components/container/UserAdmin/UserAdmin";
+import Navigation from "./components/ui/Navigation/Navigation";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <UserAdmin />
+    <BrowserRouter>
+      <div>
+        <Navigation />
+      </div>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
